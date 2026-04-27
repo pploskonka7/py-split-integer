83be6a514e907ea7515b18b0fb163167b06cd513
Piotr Ploskonka <pploskonka7@gmail.com>
2026-04-27 23:07:43 +0200

    Solution_polished

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,30 +2,24 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    result = split_integer(17, 4)
-    assert sum(result) == 17
+    assert sum(split_integer(17, 4)) == 17
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    result = split_integer(6, 2)
-    assert result == [3, 3]
+    assert split_integer(6, 2) == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    result = split_integer(6, 1)
-    assert result == [6]
+    assert split_integer(6, 1) == [6]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(17, 4)
-    assert sorted(result) == [4, 4, 4, 5]
+    assert sorted(split_integer(17,4)) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result = split_integer(0, 2)
-    assert result == [0, 0]
+    assert split_integer(0, 2) == [0, 0]
 
 
 def test_difference_between_max_and_min_number() -> None:
-    result = split_integer(17, 4)
-    assert max(result) - min(result) <= 1
+    assert max(split_integer(17, 4)) - min(split_integer(17, 4)) <= 1
